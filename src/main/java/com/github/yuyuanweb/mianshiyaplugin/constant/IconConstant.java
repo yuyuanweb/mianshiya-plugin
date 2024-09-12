1d574cac138705482a0049e08f83b6cf6d3ff7f4
pine <2268143474@qq.com>
2024-09-12 23:41:47 +0800

    chore: 更换登录及其他图标

--- src/main/java/com/github/yuyuanweb/mianshiyaplugin/constant/IconConstant.java
+++ src/main/java/com/github/yuyuanweb/mianshiyaplugin/constant/IconConstant.java
@@ -13,6 +13,12 @@ public interface IconConstant {
 
     Icon HELP = IconLoader.findIcon("/icons/help.svg", CLASS_LOADER);
 
+    Icon WEB = IconLoader.findIcon("/icons/inlayGlobe.svg", CLASS_LOADER);
+
     Icon LOGO = IconLoader.findIcon("/icons/favicon.svg", CLASS_LOADER);
+
+    Icon LOGOUT = IconLoader.findIcon("/icons/referenceBy.svg", CLASS_LOADER);
+
+    Icon LOGIN = IconLoader.findIcon("/icons/referenceTo.svg", CLASS_LOADER);
 
 }
