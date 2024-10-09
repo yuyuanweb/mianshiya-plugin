ebe27e044d1acac45cd4da4aea29c6a11f326862
pine <2268143474@qq.com>
2024-10-09 17:25:18 +0800

    feat: 添加请求 UA

--- src/main/java/com/github/yuyuanweb/mianshiyaplugin/api/interceptor/HeaderInterceptor.java
+++ src/main/java/com/github/yuyuanweb/mianshiyaplugin/api/interceptor/HeaderInterceptor.java
@@ -20,7 +20,8 @@ public class HeaderInterceptor implements Interceptor {
         Request originalRequest = chain.request();
         Request.Builder requestBuilder = originalRequest.newBuilder()
                 // 添加 cookie
-                .header("Cookie", GlobalState.getInstance().getSavedCookie());
+                .header("Cookie", GlobalState.getInstance().getSavedCookie())
+                .header("User-Agent", "jetbrains-plugin");
         Request request = requestBuilder.build();
         return chain.proceed(request);
     }
